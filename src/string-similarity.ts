3d75fae652d0fa2c11337a3a1d9bf930f6a3476d
Stephen Brown <stephenjjbrown@me.com>
2018-06-21 10:06:24 -0700

    Added JSDoc comments

--- src/string-similarity.ts
+++ src/string-similarity.ts
@@ -8,7 +8,15 @@ const getSubstrings = (str: string, substringLength: number) => {
 	return result;
 }
 
-const getStringSimilarity = (str1: string, str2: string, substringLength?: number, caseSensitive?: boolean) => {
+/**
+ * Calculate similarity between two strings
+ * @param str1 First string to match
+ * @param str2 Second string to match
+ * @param substringLength Optional. What length of substring should be used in calculating similarity. Default 2.
+ * @param caseSensitive Optional. Whether you want to consider case in string matching. Default false;
+ * @returns Number between 0 and 1, with 0 being a low match score.
+ */
+const getStringSimilarity = (str1: string, str2: string, substringLength?: number, caseSensitive?: boolean): number => {
 	if (!substringLength)
 		substringLength = 2; // Assume letter pairs
 
@@ -20,10 +28,8 @@ const getStringSimilarity = (str1: string, str2: string, substringLength?: numbe
 		str2 = str2.toLowerCase();
 	}
 
-	const substrings1 = getSubstrings(str1, substringLength);
-	const substrings2 = getSubstrings(str2, substringLength);
-
-	const union = substrings1.length + substrings2.length;
+	const substrings1 = getSubstrings(str1, substringLength), substrings2 = getSubstrings(str2, substringLength);
+	const combinedLength = substrings1.length + substrings2.length;
 	
 	let result = 0;
 	for (const x of substrings1) {
@@ -31,14 +37,14 @@ const getStringSimilarity = (str1: string, str2: string, substringLength?: numbe
 			const y = substrings2[i];
 			if (x === y) {
 				result++;
-				substrings2.splice(i, 1);
+				substrings2.splice(i, 1); // Remove from consideration in rest of loop
 				break;
 			}
 		}
 	}
 
 	if (result > 0)
-		return (result * 2) / union;
+		return (result * 2) / combinedLength;
 
 	return result;
 }

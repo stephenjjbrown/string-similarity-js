ca40d84b05c87d4b54ebcaccd6fcaf0d34094323
Stephen Brown <stephenjjbrown@me.com>
2018-06-21 13:32:51 -0700

    Added types to JSDoc comments

--- dist/string-similarity.js
+++ dist/string-similarity.js
@@ -9,10 +9,10 @@ var getSubstrings = function (str, substringLength) {
 };
 /**
  * Calculate similarity between two strings
- * @param str1 First string to match
- * @param str2 Second string to match
- * @param substringLength Optional. What length of substring should be used in calculating similarity. Default 2.
- * @param caseSensitive Optional. Whether you want to consider case in string matching. Default false;
+ * @param {string} str1 First string to match
+ * @param {string} str2 Second string to match
+ * @param {number} [substringLength=2] Optional. What length of substring should be used in calculating similarity. Default 2.
+ * @param {boolean} [caseSensitive=false] Optional. Whether you want to consider case in string matching. Default false;
  * @returns Number between 0 and 1, with 0 being a low match score.
  */
 export var getStringSimilarity = function (str1, str2, substringLength, caseSensitive) {
--- src/string-similarity.ts
+++ src/string-similarity.ts
@@ -10,10 +10,10 @@ const getSubstrings = (str: string, substringLength: number) => {
 
 /**
  * Calculate similarity between two strings
- * @param str1 First string to match
- * @param str2 Second string to match
- * @param substringLength Optional. What length of substring should be used in calculating similarity. Default 2.
- * @param caseSensitive Optional. Whether you want to consider case in string matching. Default false;
+ * @param {string} str1 First string to match
+ * @param {string} str2 Second string to match
+ * @param {number} [substringLength=2] Optional. What length of substring should be used in calculating similarity. Default 2.
+ * @param {boolean} [caseSensitive=false] Optional. Whether you want to consider case in string matching. Default false;
  * @returns Number between 0 and 1, with 0 being a low match score.
  */
 export const getStringSimilarity = (str1: string, str2: string, substringLength?: number, caseSensitive?: boolean): number => {
